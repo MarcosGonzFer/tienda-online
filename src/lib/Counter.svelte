ece6088ca656d19e464dfa4b53ed6da00bb32f23
[Jaime Blas] <[blasonic200@gmail.com]>
2024-11-05 12:29:39 +0100

    src subido

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = $state(0)
+  const sumar = () => {
+    count += 1
+  }
+  const restar = () => {
+    count -= 1
+  }
+</script>
+
+<button onclick={sumar}>
+sumar {count}
+</button>
+<button onclick={restar}>
+restar {count}
+</button>
